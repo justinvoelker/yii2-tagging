f62002d52c71d37b5dcf3a7a0336669af1f5b109
Justin Voelker <justin@justinvoelker.com>
2015-02-14 23:28:26 -0500

    Formatting for PSR standards

--- TaggingQuery.php
+++ TaggingQuery.php
@@ -2,25 +2,23 @@
 
 namespace justinvoelker\tagging;
 
-use yii\db\Query;
-
 /**
  * TaggingQuery represents a SELECT SQL statement in a way that is independent of DBMS
- * 
- * Query provides a set of methods to facilitate the specification of different clauses
- * in a SELECT statement. These methods can be chained together.
- * 
- * Though extended from yii\db\Query, portions of TaggingQuery change typical behavior.
- * Notable differences include:
+ *
+ * Query provides a set of methods to facilitate the specification of different
+ * clauses in a SELECT statement. These methods can be chained together.
+ *
+ * Though extended from yii\db\Query, portions of TaggingQuery change typical
+ * behavior. Notable differences include:
  *  - `select` must be a string, not an array of fields
- *  - `limit` and `order` attribute are not part of the initial query but instead are
- * used after the items have been selected.
- * 
- * Standard usage would include listing tags as options in a form select field or with
- * typeahead style plugins.
- * 
+ *  - `limit` and `order` attribute are not part of the initial query but
+ * instead are used after the items have been selected.
+ *
+ * Standard usage would include listing tags as options in a form select field
+ * or with typeahead style plugins.
+ *
  * For example:
- * 
+ *
  * ```php
  * $query = new TaggingQuery;
  * // compose the query
@@ -32,7 +30,7 @@ use yii\db\Query;
  *
  * @author Justin Voelker <justin@justinvoelker.com>
  */
-class TaggingQuery extends Query
+class TaggingQuery extends \yii\db\Query
 {
     /**
      * @var array key=>value pairs of tags=>frequency
@@ -43,57 +41,60 @@ class TaggingQuery extends Query
      */
     public $delimiter = ',';
     /**
-     * @var string the order used when limiting tags. Specified as key=>value pairs
-     * where keys are 'name', 'freq', or 'rand' and values are SORT_ASC, SORT_DESC, or
-     * true (true only used for 'rand'). Name sorts by tag name, Freq sorts by the
-     * frequency of the tag followed by the tag name, and Rand shuffles the order.
-     * Example: limitSort(['freq' => SORT_DESC, 'name' => SORT_ASC]) would order tags
-     * first by descending frequency and then within a given frequency by ascending name.
-     * Note that when 'name' and 'freq' are both specified, order does not matter as
-     * frequency will be the priority sort followed by name. If only one sort is
-     * specified, no other sorting will be peformed on the items.
-     * When a limit is applied, limitSort should be used to ensure the tags returned are
-     * those that are expected.
-     * Can be used in combination with displaySort in situations where the sort applied
-     * when limiting items is not the same sort applied to the final display order. For
-     * Example, selecting the 10 most frequently used tags but displaying them in
-     * alhpabetic order.
-     */    
+     * @var string the order used when limiting tags. Specified as key=>value
+     * pairs where keys are 'name', 'freq', or 'rand' and values are SORT_ASC,
+     * SORT_DESC, or true (true only used for 'rand'). Name sorts by tag name,
+     * Freq sorts by the frequency of the tag followed by the tag name, and
+     * Rand shuffles the order. Example: limitSort(['freq' => SORT_DESC,
+     * 'name' => SORT_ASC]) would order tags first by descending frequency and
+     * then within a given frequency by ascending name. Note that when 'name'
+     * and 'freq' are both specified, order does not matter as frequency will
+     * be the priority sort followed by name. If only one sort is specified, no
+     * other sorting will be peformed on the items.
+     * When a limit is applied, limitSort should be used to ensure the tags
+     * returned are those that are expected.
+     * Can be used in combination with displaySort in situations where the sort
+     * applied when limiting items is not the same sort applied to the final
+     * display order. For example, selecting the 10 most frequently used tags
+     * but displaying them in alphabetic order.
+     */
     public $limitSort;
     /**
-     * @var string the order used when displaying tags. Specified as key=>value pairs
-     * where keys are 'name', 'freq', or 'rand' and values are SORT_ASC, SORT_DESC, or
-     * true (true only used for 'rand'). Name sorts by tag name, Freq sorts by the
-     * frequency of the tag followed by the tag name, and Rand shuffles the order.
-     * Example: displaySort(['freq' => SORT_DESC, 'name' => SORT_ASC]) would order tags
-     * first by descending frequency and then within a given frequency by ascending name.
-     * Note that when 'name' and 'freq' are both specified, order does not matter as
-     * frequency will be the priority sort followed by name. If only one sort is
-     * specified, no other sorting will be peformed on the items.
-     * Can be used in combination with limitSort in situations where the sort applied
-     * when limiting items is not the same sort applied to the final display order. For
-     * Example, selecting the 10 most frequently used tags but displaying them in
-     * alhpabetic order.
+     * @var string the order used when displaying tags. Specified as key=>value
+     * pairs where keys are 'name', 'freq', or 'rand' and values are SORT_ASC,
+     * SORT_DESC, or true (true only used for 'rand'). Name sorts by tag name,
+     * Freq sorts by the frequency of the tag followed by the tag name, and
+     * Rand shuffles the order. Example: displaySort(['freq' => SORT_DESC,
+     * 'name' => SORT_ASC]) would order tags first by descending frequency and
+     * then within a given frequency by ascending name. Note that when 'name'
+     * and 'freq' are both specified, order does not matter as frequency will
+     * be the priority sort followed by name. If only one sort is specified, no
+     * other sorting will be performed on the items.
+     * Can be used in combination with limitSort in situations where the sort
+     * applied when limiting items is not the same sort applied to the final
+     * display order. For example, selecting the 10 most frequently used tags
+     * but displaying them in alphabetic order.
      */
     public $displaySort;
     /**
-     * @var array values that should be excluded from the final list of tags. All tags
-     * not in this array will be returned.
+     * @var array values that should be excluded from the final list of tags.
+     * All tags not in this array will be returned.
      */
     public $exclude;
     /**
-     * @var array values that should the only values included in the final list of tags.
-     * All values not in this array will be removed.
+     * @var array values that should the only values included in the final list
+     * of tags. All values not in this array will be removed.
      */
     public $includeOnly;
     /**
-    * @var string the Query limit will be not be used during initial selection of
-    * records. It will be used to limit the final list of items. Original limit will be
-    * set to null for initial selection with the original value stored in this private
-    * attribute. This private attribute will be used to limit the final items.
-    */
+     * @var string the Query limit will be not be used during initial selection
+     * of records. It will be used to limit the final list of items. Original
+     * limit will be set to null for initial selection with the original value
+     * stored in this private attribute. This private attribute will be used to
+     * limit the final items.
+     */
     private $_limit;
-    
+
     public function getTags($db = null)
     {
         // $items will only be an array if provided from earlier instance
@@ -115,16 +116,22 @@ class TaggingQuery extends Query
     }
 
     /**
-    * Select data from database as specified by the criteria, merges all results into
-    * single array after being split be delimiter, and finally counts the frequency of
-    * all of the values resulting in an array of key=>value pairs of tag=>frequency.
-    */
-    public function getItems($db = null) {
+     * Select data from database as specified by the criteria, merges all
+     * results into single array after being split be delimiter, and finally
+     * counts the frequency of all of the values resulting in an array of
+     * key=>value pairs of tag=>frequency.
+     * @param Connection $db the database connection used to generate the SQL
+     * statement. If this parameter is not given, the `db` application
+     * component will be used.
+     * @return array items as key=>value (tag=>frequency)
+     */
+    public function getItems($db = null)
+    {
         $items = [];
         $rows = $this->createCommand($db)->queryAll();
         foreach ($rows as $row) {
             foreach ($this->select as $selectField) {
-                $items = array_merge($items,explode($this->delimiter,$row[$selectField]));
+                $items = array_merge($items, explode($this->delimiter, $row[$selectField]));
             }
         }
         $items = array_count_values(array_filter($items));
@@ -132,9 +139,11 @@ class TaggingQuery extends Query
     }
 
     /**
-    * Reduce the list of items according to exclude and includeOnly criteria
-    */
-    public function reduceItems() {
+     * Reduce the list of items according to exclude and includeOnly criteria
+     * @return array item list reduced by exclude or includeOnly values
+     */
+    public function reduceItems()
+    {
         $items = $this->items;
         if (!empty($this->exclude)) {
             $items = array_diff_key($items, array_fill_keys($this->exclude, ''));
@@ -146,11 +155,14 @@ class TaggingQuery extends Query
     }
 
     /**
-    * Sort the list of items according to criteria
-    */
-    public function sortItems($sort) {
-       $items = $this->items;
-       if (is_array($sort)) {
+     * Sort the list of items according to criteria
+     * @param array value to be sorted by and the order to be sorted (orderBy=>order)
+     * @return array sorted item list
+     */
+    public function sortItems($sort)
+    {
+        $items = $this->items;
+        if (is_array($sort)) {
             if (array_key_exists('freq', $sort) && array_key_exists('name', $sort)) {
                 array_multisort(array_values($items), $sort['freq'], array_keys($items), $sort['name'], $items);
             } elseif (array_key_exists('freq', $sort)) {
@@ -165,50 +177,99 @@ class TaggingQuery extends Query
     }
 
     /**
-    * Limit the list of items according to the original limit criteria
-    */
-    public function limitItems() {
+     * Limits the final results by limit specified
+     * @return array limited item list
+     */
+    public function limitItems()
+    {
         return array_slice($this->items, 0, $this->_limit);
     }
-    
+
+    /**
+     * Sets items to be used if starting from existing set of values.
+     * @param array $items existing items as key=>value (tag=>frequency)
+     * @return array the query object itself
+     */
     public function items($items)
     {
         $this->items = $items;
         return $this;
     }
+
+    /**
+     * Sets the delimiter used the separate individual tags
+     * @param string $delimiter as
+     * @return static the query object itself
+     */
     public function delimiter($delimiter)
     {
         $this->delimiter = $delimiter;
         return $this;
     }
+
+    /**
+     * Sets the LIMIT part of the query to null while preserving the intended limit
+     * @param integer $limit the limit. Use null or negative value to disable limit.
+     * @return static the query object itself
+     */
     public function limit($limit)
     {
         $this->limit = null;
         $this->_limit = $limit;
         return $this;
     }
+
+    /**
+     * Sets the sorting to be used when applying the limit
+     * @param array the key=>value of orderBy=>order
+     * @return static the query object itself
+     */
     public function limitSort($limitSort)
     {
         $this->limitSort = $limitSort;
         return $this;
     }
+
+    /**
+     * Sets the sorting to be used for final presentation
+     * @param array the key=>value of orderBy=>order
+     * @return static the query object itself
+     */
     public function displaySort($displaySort)
     {
         $this->displaySort = $displaySort;
         return $this;
     }
+
+    /**
+     * Sets the items to be excluded from final results
+     * @param array key-only array of values to exclude
+     * @return static the query object itself
+     */
     public function exclude($exclude)
     {
         $this->exclude = $exclude;
         return $this;
     }
+
+    /**
+     * Sets the items to be included in the final results (all others excluded)
+     * @param array key-only array of values to include
+     * @return static the query object itself
+     */
     public function includeOnly($includeOnly)
     {
         $this->includeOnly = $includeOnly;
         return $this;
     }
 
-    function shuffle_assoc($items) { 
+    /**
+     * Shuffles the items into a random order
+     * @param array $items items as key=>value (tag=>frequency)
+     * @return static the randomized list of items
+     */
+    function shuffle_assoc($items)
+    {
         $random = [];
         $keys = array_keys($items);
         shuffle($keys);
@@ -216,5 +277,5 @@ class TaggingQuery extends Query
             $random[$key] = $items[$key];
         }
         return $random;
-    } 
+    }
 }
--- TaggingWidget.php
+++ TaggingWidget.php
@@ -2,14 +2,13 @@
 
 namespace justinvoelker\tagging;
 
-use yii\base\Widget;
 use yii\helpers\Html;
 
 /**
  * TaggingWidget renders an HTML tag cloud or as an ordered/unordered list
- * 
+ *
  * For example:
- * 
+ *
  * ```php
  * echo TaggingWidget::widget([
  *     'items' => $tags,
@@ -20,12 +19,13 @@ use yii\helpers\Html;
  *
  * @author Justin Voelker <justin@justinvoelker.com>
  */
-class TaggingWidget extends Widget
+class TaggingWidget extends \yii\base\Widget
 {
     /**
-     * @var array key=>value pairs of tags=>frequency. If multiple TaggingWidgets will
-     * use the same data on the same page, TaggingQuery can be used to first generate
-     * the item list which can then be passed to TaggingWidget for display.
+     * @var array key=>value pairs of tags=>frequency. If multiple
+     * TaggingWidgets will use the same data on the same page, TaggingQuery can
+     * be used to first generate the item list which can then be passed to
+     * TaggingWidget for display.
      */
     public $items;
     /**
@@ -41,40 +41,41 @@ class TaggingWidget extends Widget
      */
     public $unit = 'px';
     /**
-     * @var string format of the returned tags. Options are 'cloud', 'ul',  or 'ol'.
-     * Cloud will return a tag cloud with font-sizes adjusted according to their count
-     * (frequency). 'ul' and 'ol' will return the appropriate list that can be
-     * formatted as desired. If 'ol' is specified, a 'type' will likely be desired as
-     * a 'ulOptions' value.
+     * @var string format of the returned tags. Options are 'cloud', 'ul', or
+     * 'ol'. Cloud will return a tag cloud with font-sizes adjusted according
+     * to their count (frequency). 'ul' and 'ol' will return the appropriate
+     * list that can be formatted as desired. If 'ol' is specified, a 'type'
+     * will likely be desired as a 'ulOptions' value.
      */
     public $format = 'cloud';
     /**
-     * @var string the route that will be used as a base onto which 'urlParam' and the
-     * tag name will be appended.
+     * @var string the route that will be used as a base onto which 'urlParam'
+     * and the tag name will be appended.
      */
     public $url;
     /**
-     * @var string the URL parameter that will be used with the tag and appended to URL.
+     * @var string the URL parameter that will be used with the tag and
+     * appended to URL.
      */
     public $urlParam;
     /**
-     * @var string options that are to be assigned to the ul or ol in the tag cloud or
-     * list.
+     * @var string options that are to be assigned to the ul or ol in the tag
+     * cloud or list.
      */
     public $listOptions = array();
     /**
-     * @var string options that are to be assigned to each list item (li) in the tag
-     * cloud or list.
+     * @var string options that are to be assigned to each list item (li) in
+     * the tag cloud or list.
      */
     public $liOptions = array();
     /**
-     * @var string options that are to be assigned to each link (a) in the tag cloud or
-     * list.
+     * @var string options that are to be assigned to each link (a) in the tag
+     * cloud or list.
      */
     public $linkOptions = array();
     /**
-     * @var array the minimum frequency found for all tags which is used as the basis
-     * for increasing the font-size of more frequent tags.
+     * @var array the minimum frequency found for all tags which is used as the
+     * basis for increasing the font-size of more frequent tags.
      */
     private $_minCount;
     /**
@@ -83,17 +84,20 @@ class TaggingWidget extends Widget
     private $_fontStep = 1;
 
     /**
-    * Renders the widget
-    */
+     * Renders the widget
+     * @return string the rendering result of the widget.
+     */
     public function run()
     {
         return $this->renderItems();
     }
 
     /**
-    * Renders widget items
-    */
-    public function renderItems() {
+     * Renders widget items
+     * @return string complete list of rendered items
+     */
+    public function renderItems()
+    {
         if (!empty($this->items)) {
             $this->_fontStep = $this->getFontStep();
         }
@@ -107,16 +111,20 @@ class TaggingWidget extends Widget
             Html::addCssClass($this->listOptions, 'tagging_list');
         }
         $listType = ($this->format == 'ol') ? 'ol' : 'ul';
-        return Html::tag($listType, implode("\n", $items), $this->listOptions);            
+        return Html::tag($listType, implode("\n", $items), $this->listOptions);
 
     }
 
     /**
-    * Renders a widget's item
-    */
-    public function renderItem($name, $count) {
+     * Renders a widget's item
+     * @param string the item name to be displayed
+     * @param the count or frequency of the item to be displayed
+     * @return string a single item within the complete list
+     */
+    public function renderItem($name, $count)
+    {
         $fontSize = ($this->smallest + (($count - $this->_minCount) * $this->_fontStep));
-        if (!empty($this->url)){
+        if (!empty($this->url)) {
             $url = $this->url;
             $url[] = [$this->urlParam => $name];
         }
@@ -131,16 +139,20 @@ class TaggingWidget extends Widget
     }
 
     /**
-    * Determine the step size for font increases
-    */
-    public function getFontStep(){
+     * Determine the step size for font increases
+     * @return decimal size of individual font step for increasingly frequent items
+     */
+    public function getFontStep()
+    {
         $this->_minCount = min($this->items);
         $spread = max($this->items) - $this->_minCount;
-        if ( $spread <= 0 )
+        if ($spread <= 0) {
             $spread = 1;
+        }
         $font_spread = $this->largest - $this->smallest;
-        if ( $font_spread < 0 )
+        if ($font_spread < 0) {
             $font_spread = 1;
+        }
         return $font_spread / $spread;
     }
 }
